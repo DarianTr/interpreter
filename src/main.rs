773c1cf05b6913a825c97bc3d0cd754a2e940c88
DarianTr <dlt7@unamail.net>
2023-06-29 07:02:37 +0200

    clap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use clap::Parser;
 mod parser;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    file: String,
+}
 fn main() {
-    let x = parser::parser("test.txt".to_owned());
+    let args = Args::parse();
+    println!("{:?}", args);
+    let x = parser::parser(args.file);
     println!("{:?}", x);
 }
